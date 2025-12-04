83b3d7f9668f3b07751d95c098bff62916510ec6
lovecrimecode <zelideeguemez@gmail.com>
2025-12-03 21:38:54 -0400

    implemented search, details and read functions

--- app/Http/Controllers/Api/BookApiController.php
+++ app/Http/Controllers/Api/BookApiController.php
@@ -15,4 +15,36 @@ class BookApiController extends Controller
     public function show(Book $book) {
         return $book;
     }
+
+    public function search (Request $request) {
+        $query = urlemcode($request->input('q'));
+
+        $response = Http::get(("https://openlibrary.org/search.json?q={$query}");
+
+        return $responde->json()['docs'] ?? [];
+    }
+
+    public function details($workId) {
+        $response = Http::get("https://openlibrary.org/works/{$workId}.json");
+
+        return $response->json() ?? [];
+    }
+
+    public function read($editionId) {
+        $response = Http::get("https://openlibrary.org/books/{$editionId}.json");
+
+        if (isset($edition['formats']['text/html'])) {
+            $htmlUrl = $edition['formats']['text/html'];
+            $response = Http::get($htmlUrl);
+            return $response->body();
+        }
+
+        if (isset($edition['formats']['text/plain'])) {
+            $htmlUrl = $edition['formats']['text/plain'];
+            $response = Http::get($htmlUrl);
+            return $response->body();
+        }
+
+        return "Content not available in reading format.";
+    }
 }
