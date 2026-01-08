a6eca2ef803325baefb8172901889d50888fd9a5
lovecrimecode <zelideeguemez@gmail.com>
2026-01-08 00:49:58 -0400

    feature: reading progress, purchase and book models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,6 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     //use HasFactory;
+    protected $fillable = [
+        'work_id',
+        'title',
+        'authors',
+        'cover_url',
+        'description',
+        'published_year',
+    ];
 
     public function buyers()
     {
